928017fdc7088dcf7b24c54e94310ec9e9a8fc0c
Pontax02 <ppontanilla07@gmail.com>
2025-08-31 18:14:00 +0200

    test(test_bowling): :fire: Eliminar casos test extra

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -31,20 +31,3 @@ def test_score_spare():                # PINS == 121
     PINS = "9-3/613/815/-/8-7/8-"
     card = ScoreCard(PINS)
     assert 121 == card.computeScore()
-
-
-def test_extra_rolls():                # PINS == 131
-    PINS = "9-3/613/815/-/8-7/8/8"
-    card = ScoreCard(PINS)
-    assert 131 == card.computeScore()
-
-def test_spare_in_extra_rolls():                # PINS == 175
-    PINS = "X5/X5/XX5/--5/X5/"
-    card = ScoreCard(PINS)
-    assert 175 == card.computeScore()
-    
-
-def test_full_strikes():                # PINS == 300
-    PINS = "XXXXXXXXXXXX"
-    card = ScoreCard(PINS)
-    assert 300 == card.computeScore()
